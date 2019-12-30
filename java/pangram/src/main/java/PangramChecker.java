a3b4b67f73d43a85cdd2afd794ad2df4eeb91d98
manikantapanuganti <panugantimanikantachari@gmail.com>
2019-12-30 10:04:59 +0530

    Snake and Ladders game

--- java/pangram/src/main/java/PangramChecker.java
+++ java/pangram/src/main/java/PangramChecker.java
@@ -1,7 +1,15 @@
+import java.util.*;
 public class PangramChecker {
 
     public boolean isPangram(String input) {
        // throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        String s="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        char[] array=s.toCharArray();
+        List<Character> li=new ArrayList<Character>();
+        for(char a:array){
+            li.add(a);
+        }
+
     }
 
 }
