09b43a5e0068d840a8d69a9500ecea800576ab82
manikantapanuganti <panugantimanikantachari@gmail.com>
2019-12-30 11:08:16 +0530

    Adding pangram solutions

--- java/pangram/src/main/java/PangramChecker.java
+++ java/pangram/src/main/java/PangramChecker.java
@@ -3,13 +3,25 @@ public class PangramChecker {
 
     public boolean isPangram(String input) {
        // throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-        String s="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String s="abcdefghijklmnopqrstuvwxyz";
         char[] array=s.toCharArray();
         List<Character> li=new ArrayList<Character>();
         for(char a:array){
             li.add(a);
         }
+        input=input.toLowerCase();
+        array=input.toCharArray();
+        for(char a:array){
+            li.remove(new Character(a));
+        }
+       // System.out.println("Is pangram"+li.isEmpty());
 
+
+
+        return li.isEmpty();
     }
-
+   /* public static void main(String[] args){
+        PangramChecker pc=new PangramChecker();
+        pc.isPangram("abcdefghijklnopqrstuvwxyz");
+    }*/
 }
